7ef6a6c9191281ed75d923568add9ee15aecd851
arun <arun.inoble@gmail.com>
2025-01-08 00:10:26 +0530

    Remove Unused Imports

--- src/main/java/com/retail/entity/Customers.java
+++ src/main/java/com/retail/entity/Customers.java
@@ -2,11 +2,9 @@ package com.retail.entity;
 
 import com.retail.util.ApplicationConstants;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
