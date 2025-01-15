aee92e215134ead3f90f9897c69b279ff5a85025
arun <arun.inoble@gmail.com>
2025-01-15 15:35:18 +0530

    Controller Test class,Gitignore for logs

---
+++ src/test/java/com/retail/CustomerControllerTests.java
@@ -0,0 +1,99 @@
+package com.retail;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.retail.controller.CustomerController;
+import com.retail.entity.Customers;
+import com.retail.service.CustomerService;
+import com.retail.util.ApplicationConstants;
+import com.retail.util.ResponsePojo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerControllerTests {
+
+    @InjectMocks
+    private CustomerController controller;
+
+    @Mock
+    private CustomerService customerService;
+
+    private MockMvc mockMvc;
+    @Test
+    public void testCreateCustomer() throws Exception {
+        Customers customers = new Customers("1234", "name", "9876543210", new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ApplicationConstants.ADMIN_USER);
+        when(customerService.saveCustomer(any(Customers.class))).thenReturn(new ResponsePojo(ApplicationConstants.SUCCESS, "Customer Saved Successfully!", customers));
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/customer/saveCustomer")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(customers))
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ApplicationConstants.SUCCESS));
+
+    }
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+    }
+
+
+
+    @Test
+    public void testCreateCustomer_InputValidationScenarios() throws Exception {
+        // Scenario 1 - null Name values
+        Customers customers = new Customers("1234", null, "9876543210", new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ApplicationConstants.ADMIN_USER);
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/customer/saveCustomer")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(customers))
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        MethodArgumentNotValidException methodArgumentNotValidException = (MethodArgumentNotValidException) result.getResolvedException();
+
+        assertNotNull(methodArgumentNotValidException);
+        assertNotNull(methodArgumentNotValidException.getFieldErrors());
+        assertEquals("name", methodArgumentNotValidException.getFieldErrors().get(0).getField());
+        assertEquals("must not be null", methodArgumentNotValidException.getFieldErrors().get(0).getDefaultMessage());
+
+
+
+        // Scenario 2 - Invalid Mobile number
+        customers = new Customers("1234", "test", "987654321", new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), ApplicationConstants.ADMIN_USER);
+        request = MockMvcRequestBuilders
+                .post("/api/customer/saveCustomer")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(customers))
+                .contentType(MediaType.APPLICATION_JSON);
+        result = mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        methodArgumentNotValidException = (MethodArgumentNotValidException) result.getResolvedException();
+        assertNotNull(methodArgumentNotValidException);
+        assertNotNull(methodArgumentNotValidException.getFieldErrors());
+        assertEquals("mobile", methodArgumentNotValidException.getFieldErrors().get(0).getField());
+        assertEquals("Mobile number should be between 10-15 numbers", methodArgumentNotValidException.getFieldErrors().get(0).getDefaultMessage());
+    }
+}
---
+++ src/test/java/com/retail/PurchaseOrderControllerTests.java
@@ -0,0 +1,161 @@
+package com.retail;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.retail.controller.PurchaseOrderController;
+import com.retail.entity.PurchaseOrders;
+import com.retail.service.PurchaseOrderService;
+import com.retail.util.ApplicationConstants;
+import com.retail.util.ResponsePojo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PurchaseOrderControllerTests {
+    @InjectMocks
+    private PurchaseOrderController controller;
+
+    @Mock
+    private PurchaseOrderService purchaseOrderService;
+
+    private MockMvc mockMvc;
+
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    public void testCreatePurchaseOrder() throws Exception {
+        PurchaseOrders purchaseOrders = new PurchaseOrders("customerId", new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0);
+        when(purchaseOrderService.savePurchaseOrder(any(PurchaseOrders.class))).thenReturn(new ResponsePojo(ApplicationConstants.SUCCESS, "Purchase Order Saved!", purchaseOrders));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/purchaseOrder/savePurchaseOrder")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(purchaseOrders))
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ApplicationConstants.SUCCESS));
+
+    }
+
+    @Test
+    public void testCreatePurchaseOrder_InputValidationScenarios() throws Exception {
+
+        // Scenario 1 - null customer Id
+        PurchaseOrders purchaseOrders = new PurchaseOrders(null, new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0);
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/purchaseOrder/savePurchaseOrder")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(purchaseOrders))
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        MethodArgumentNotValidException methodArgumentNotValidException = (MethodArgumentNotValidException) result.getResolvedException();
+
+        assertNotNull(methodArgumentNotValidException);
+        assertNotNull(methodArgumentNotValidException.getFieldErrors());
+        assertEquals("customerId", methodArgumentNotValidException.getFieldErrors().get(0).getField());
+        assertEquals("must not be null", methodArgumentNotValidException.getFieldErrors().get(0).getDefaultMessage());
+
+        // Scenario 2 - null Order Date
+        purchaseOrders = new PurchaseOrders("test", null, 120.0);
+        request = MockMvcRequestBuilders
+                .post("/api/purchaseOrder/savePurchaseOrder")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(purchaseOrders))
+                .contentType(MediaType.APPLICATION_JSON);
+        result = mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        methodArgumentNotValidException = (MethodArgumentNotValidException) result.getResolvedException();
+
+        assertNotNull(methodArgumentNotValidException);
+        assertNotNull(methodArgumentNotValidException.getFieldErrors());
+        assertEquals("orderDate", methodArgumentNotValidException.getFieldErrors().get(0).getField());
+        assertEquals("must not be null", methodArgumentNotValidException.getFieldErrors().get(0).getDefaultMessage());
+
+        // Scenario 3 - null Order Value
+        purchaseOrders = new PurchaseOrders("test", new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), null);
+        request = MockMvcRequestBuilders
+                .post("/api/purchaseOrder/savePurchaseOrder")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(purchaseOrders))
+                .contentType(MediaType.APPLICATION_JSON);
+        result = mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        methodArgumentNotValidException = (MethodArgumentNotValidException) result.getResolvedException();
+
+        assertNotNull(methodArgumentNotValidException);
+        assertNotNull(methodArgumentNotValidException.getFieldErrors());
+
+        assertEquals("orderTotal", methodArgumentNotValidException.getFieldErrors().get(0).getField());
+        assertEquals("must not be null", methodArgumentNotValidException.getFieldErrors().get(0).getDefaultMessage());
+
+
+    }
+
+    @Test
+    public void testBulkProcessPurchaseOrders() throws Exception {
+        List<PurchaseOrders> purchaseOrdersList = Arrays.asList(
+                new PurchaseOrders("C1", convertStringToDate("2025-01-01").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0),
+                new PurchaseOrders("C1", convertStringToDate("2025-01-05").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0));
+
+        when(purchaseOrderService.bulkProcessPurchaseOrders()).thenReturn(new ResponsePojo(ApplicationConstants.SUCCESS, "Purchase Order Saved!", purchaseOrdersList));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/api/purchaseOrder/bulkProcessPurchaseOrders");
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ApplicationConstants.SUCCESS))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.length()").value(2));
+
+    }
+
+    @Test
+    public void testFetchOrderHistory() throws Exception {
+        List<PurchaseOrders> purchaseOrdersList = Arrays.asList(
+                new PurchaseOrders("C1", convertStringToDate("2025-01-01").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0),
+                new PurchaseOrders("C1", convertStringToDate("2025-01-05").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), 120.0));
+        Map<String, Object> responseMap = new LinkedHashMap<>();
+        responseMap.put(ApplicationConstants.CUSTOMER_NAME, "test");
+        responseMap.put(ApplicationConstants.CUSTOMER_MOBILE, "9876543210");
+        responseMap.put(ApplicationConstants.TOTAL_ORDERS, purchaseOrdersList.size());
+        responseMap.put(ApplicationConstants.TOTAL_ORDERS_VALUE, 240);
+        responseMap.put(ApplicationConstants.TOTAL_REWARD_POINTS, 180);
+        responseMap.put(ApplicationConstants.ORDERS, purchaseOrdersList);
+        when(purchaseOrderService.fetchOrderHistory("customerId",null,
+                null,false)).thenReturn(new ResponsePojo(ApplicationConstants.SUCCESS, "Details Fetched Successfully!", responseMap));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/api/purchaseOrder/fetchOrderHistory").header("customerId","customerId");
+         mockMvc.perform(request).andExpect(MockMvcResultMatchers.jsonPath("$.status").value(ApplicationConstants.SUCCESS))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Orders.length()").value(2));
+
+    }
+
+    Date convertStringToDate(String date) throws ParseException {
+        Date convertedDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        return convertedDate;
+    }
+}
